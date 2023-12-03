06916eb5ff12670131c981f975f36a4ab9d38e26
Afrah Ali <116621717+Afrahbalushi@users.noreply.github.com>
2023-12-03 15:18:59 +0400

    my digital clock

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const boxStyleObj = { 
+    height:300, 
+    width:1100, 
+    marginTop:100,
+    fontSize: 200
+  }
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalTime = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalTime);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm">
+        <h1 className='App'>Digital Clock</h1>
+        <button type="button" className="btn btn-outline-primary" style={boxStyleObj}>{formattedTime}</button>
+        </div>
+      </div>
     </div>
+   
   );
 }
 
